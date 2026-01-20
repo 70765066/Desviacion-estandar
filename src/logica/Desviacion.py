ea1b92d96ca02df5bb52f816f315ee7d6004166e
70765066 <70765067@continental.edu.pe>
2026-01-19 19:26:35 -0500

    Caso 3: dos elementos (Verde)

--- src/logica/Desviacion.py
+++ src/logica/Desviacion.py
@@ -1,4 +1,6 @@
 # src/logica/Desviacion.py
+import math
+
 class Desviacion:
     def __init__(self, datos):
         self.__datos = datos
@@ -10,3 +12,19 @@ class Desviacion:
 
         if len(self.__datos) == 1:
             return 0
+
+
+    def calcular(self):
+        n = len(self.__datos)
+
+        if n == 0:
+            return None
+        if n == 1:
+            return 0
+
+        promedio = sum(self.__datos) / n
+        suma = 0
+        for x in self.__datos:
+            suma += (x - promedio) ** 2
+
+        return math.sqrt(suma / n)
